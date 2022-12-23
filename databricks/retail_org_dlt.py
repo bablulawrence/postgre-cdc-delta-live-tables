a36f886cfcba1db7c200feb96acf638be1853885
bablulawrence <bablulawrence>
2022-12-23 11:30:40 +0000

    Renamed retail_org notebook

--- databricks/retail_org_dlt.py
+++ databricks/retail_org_dlt.py
@@ -179,7 +179,7 @@ sales_orders_schema = StructType(
 def sales_orders_cleansed():
     return (
         dlt.read_stream("sales_orders_raw")
-#         spark.readStream.format("delta").table("retail_org.sales_orders_raw")
+        #         spark.readStream.format("delta").table("retail_org.sales_orders_raw")
         .select(get_json_object(col("value"), "$.payload.after").alias("row"))
         .withColumn("row", regexp_replace("row", '"\\[', "["))
         .withColumn("row", regexp_replace("row", '\\]"', "]"))
@@ -192,6 +192,7 @@ def sales_orders_cleansed():
         .withColumn("unit_price", col("ordered_products").price)
         .withColumn("quantity", col("ordered_products").qty)
     )
+
 
 # sales_orders_cleansed().display()
 
@@ -255,7 +256,7 @@ customers_schema = StructType(
 def customers_cleansed():
     return (
         dlt.read_stream("customers_raw")
-#         spark.readStream.format("delta").table("retail_org.customers_raw")
+        #         spark.readStream.format("delta").table("retail_org.customers_raw")
         #          spark.read.format("delta").table("retail_org.customers_raw")
         .select(get_json_object(col("value"), "$.payload.after").alias("row"))
         .select(from_json(col("row"), customers_schema).alias("row"))
@@ -300,7 +301,7 @@ products_schema = StructType(
 def products_cleansed():
     return (
         dlt.read_stream("products_raw")
-#         spark.readStream.format("delta").table("retail_org.products_raw")
+        #         spark.readStream.format("delta").table("retail_org.products_raw")
         #         spark.read.format("delta").table("retail_org.products_raw")
         .select(get_json_object(col("value"), "$.payload.after").alias("row"))
         .select(from_json(col("row"), products_schema).alias("row"))
@@ -312,7 +313,169 @@ def products_cleansed():
 
 # COMMAND ----------
 
-# MAGIC %sql --DESCRIBE TABLE retail_org.sales_orders_tmp
-# MAGIC --SHOW CREATE TABLE retail_org.sales_orders_tmp
-# MAGIC -- SELECT * FROM retail_org.sales_orders_cleansed
-# MAGIC -- SELECT * FROM retail_org.products_cleansed
+# CREATE LIVE TABLE dim_products (
+#   product_key BIGINT GENERATED ALWAYS AS identity,
+#   product_id STRING,
+#   product_category STRING,
+#   product_name STRING,
+#   sales_price STRUCT<scale: INT, value: STRING>,
+#   ean13 DOUBLE,
+#   ean5 STRING,
+#   product_unit STRING
+# ) COMMENT "Products dimension." TBLPROPERTIES ("quality" = "gold") AS
+# SELECT
+#   *
+# FROM
+#   retail_org.products_silver
+
+
+@dlt.table(
+    schema="""
+         product_key BIGINT GENERATED ALWAYS AS identity,
+         product_id STRING,
+         product_category STRING,
+         product_name STRING,
+         sales_price STRUCT<scale: INT, value: STRING>,
+         ean13 DOUBLE,
+         ean5 STRING,
+         product_unit STRING    
+    """,
+    comment="Load data to products dimension table",
+    table_properties={"quality": "gold", "pipelines.reset.allowed": "true"},
+    spark_conf={"pipelines.trigger.interval": "60 seconds"},
+    temporary=False,
+)
+def dim_products():
+    return dlt.read_stream("products_cleansed")
+
+# COMMAND ----------
+
+# CREATE LIVE TABLE dim_customers (
+#  customer_key BIGINT GENERATED ALWAYS AS IDENTITY,
+#  customer_id BIGINT,
+#   tax_id STRING,
+#   tax_code STRING,
+#   customer_name STRING,
+#   state STRING,
+#   city STRING,
+#   postcode STRING,
+#   street STRING,
+#   number STRING,
+#   unit STRING,
+#   region STRING,
+#   district STRING,
+#   lon DECIMAL(10,6),
+#   lat DECIMAL(10,6),
+#   ship_to_address STRING,
+#   valid_from STRUCT<scale: INT, value: STRING>,
+#   valid_to STRUCT<scale: INT, value: STRING>,
+#   units_purchased STRUCT<scale: INT, value: STRING>,
+#   loyalty_segment STRING)
+#   COMMENT "Customer dimension." TBLPROPERTIES ("quality" = "gold") AS
+#   SELECT * FROM retail_org.customers_silver
+
+
+@dlt.table(
+    schema="""
+          customer_key BIGINT GENERATED ALWAYS AS IDENTITY, 
+          customer_id BIGINT,
+          tax_id STRING,
+          tax_code STRING,
+          customer_name STRING,
+          state STRING,
+          city STRING,
+          postcode STRING,
+          street STRING,
+          number STRING,
+          unit STRING,
+          region STRING,
+          district STRING,
+          lon DECIMAL(10,6),
+          lat DECIMAL(10,6),
+          ship_to_address STRING,
+          valid_from STRUCT<scale: INT, value: STRING>,
+          valid_to STRUCT<scale: INT, value: STRING>,
+          units_purchased STRUCT<scale: INT, value: STRING>,
+          loyalty_segment STRING
+    """,
+    comment="Load data to customers dimension table",
+    table_properties={"quality": "gold", "pipelines.reset.allowed": "true"},
+    spark_conf={"pipelines.trigger.interval": "60 seconds"},
+    temporary=False,
+)
+def dim_customers():
+    return dlt.read_stream("customers_cleansed")
+
+# COMMAND ----------
+
+# CREATE STREAMING LIVE TABLE fact_sales_orders
+# -- CREATE OR REPLACE TEMPORARY VIEW fact_sales_orders
+# AS
+# SELECT s.order_number, c.customer_key, p.product_key, cast(s.order_datetime as date) order_date, s.unit_price, s.quantity, (s.unit_price * s.quantity) AS total_price   FROM retail_org.sales_orders_silver s
+# INNER JOIN retail_org.dim_products p ON s.product_id = p.product_id
+# INNER JOIN retail_org.dim_customers c ON s.customer_id = c.customer_id;
+
+
+@dlt.table(
+    comment="load data to sales orders fact table",
+    table_properties={"quality": "gold", "pipelines.reset.allowed": "true"},
+    spark_conf={"pipelines.trigger.interval": "60 seconds"},
+    temporary=False,
+)
+def fact_sales_orders():
+    s = dlt.read_stream("sales_orders_cleansed").alias("s")
+    p = dlt.read_stream("dim_products").alias("p")
+    c = dlt.read_stream("dim_customers").alias("c")
+    return (
+        s.join(p, s.product_id == p.product_id, "inner")
+        .join(c, s.customer_id == c.customer_id, "inner")
+        .select(
+            "s.order_number",
+            "c.customer_key",
+            "p.product_key",
+            col("s.order_datetime").cast("date"),
+            "s.unit_price",
+            "s.quantity",
+            expr("s.unit_price * s.quantity").alias("total_price"),
+        )
+    )
+
+# COMMAND ----------
+
+# CREATE LIVE TABLE customer_sales
+# -- CREATE TEMPORARY VIEW customer_sales
+# AS
+# SELECT customer_key, product_key, sum(quantity) total_quantity, sum(total_price) sale_amount
+# FROM sales_orders_fact_tmp
+# GROUP BY customer_key, product_key
+
+
+@dlt.table(
+    comment="load data to customer sales fact table",
+    table_properties={"quality": "gold", "pipelines.reset.allowed": "true"},
+    spark_conf={"pipelines.trigger.interval": "60 seconds"},
+    temporary=False,
+)
+def fact_customer_sales():
+    s = dlt.read("sales_orders_cleansed").alias("s")
+    p = dlt.read("dim_products").alias("p")
+    c = dlt.read("dim_customers").alias("c")
+    return (
+        s.join(p, s.product_id == p.product_id, "inner")
+        .join(c, s.customer_id == c.customer_id, "inner")
+        .groupBy("s.product_id", "c.customer_id")
+        .agg(
+            sum("quantity").alias("total_quantity"),
+            sum(expr("s.unit_price * s.quantity")).alias("sale_amount"),
+        )
+    )
+
+# COMMAND ----------
+
+# MAGIC %sql --DESCRIBE TABLE retail_org.fact_sales_orders
+# MAGIC --SHOW CREATE TABLE retail_org.fact_sales_orders
+# MAGIC -- SELECT * FROM retail_org.fact_sales_orders
+# MAGIC -- SELECT * FROM retail_org.fact_customer_sales
+# MAGIC -- SELECT * FROM retail_org.dim_products
+# MAGIC -- SELECT * FROM retail_org.dim_customers
+# MAGIC -- SELECT * FROM retail_org.customers_raw
